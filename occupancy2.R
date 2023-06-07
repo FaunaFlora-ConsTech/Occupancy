40a8215091a372efe460bc40f4c8c8faf72794f5
uttp <95093815+ugnpnjr@users.noreply.github.com>
2023-06-07 10:56:01 +0100

    Update occupancy2.R
    Added codes to make a plot using visreg package.

--- occupancy2.R
+++ occupancy2.R
@@ -332,4 +332,12 @@ plot(mean ~ elevation, pred.glm, type="l", lwd=2, ylim=c(0,1),
 lines(lower ~ elevation, pred.glm, type="l", col=gray(0.5))
 lines(upper ~ elevation, pred.glm, type="l", col=gray(0.5))
 
+# Or using visreg package
+visreg(fit=glm.mod1, xvar="elevation", 
+       gg = TRUE, 
+       scale="response") +
+  labs(y = "Habitat use prob", 
+       x = "Elevation (standardised)")
+
+
 ##################################### END ##################################
